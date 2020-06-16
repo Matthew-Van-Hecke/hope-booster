5dca1841b0d8771dddf2bdb75c77b31cdee4923d
Matthew VH <vanheckematt@gmail.com>
2020-06-16 12:04:56 -0500

    Added a submitResponses function to Quiz. This takes the responses object from the state, collects all of the individual responses contained therein and makes an array of them. This will eventually allow for comparing of responses to the array attached to each possible result.

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,25 +8,40 @@ class Quiz extends Component{
         super(props);
         this.state = {responses: {}};
         this.getResponse = this.getResponse.bind(this);
+        this.submitResponses = this.submitResponses.bind(this);
     }
     getResponse(data){
         let responses = {...this.state.responses};
         responses[data.question] = data.selected;
         this.setState({responses});
-        console.log(data);
+    }
+    submitResponses(){
+        let responseArrays = Object.values(this.state.responses);
+        let responses = [];
+        for(let i = 0; i < responseArrays.length; i++){
+            for(let j = 0; j < responseArrays[i].length; j++){
+                responses.push(responseArrays[i][j]);
+            }
+        }
+        console.log(responses);
     }
     render(){
         return(
-            QuestionsDb.map(
-                q => 
-                <Question
-                    key={q.question}
-                    question={q.question}
-                    options={q.options}
-                    selectOne={q.selectOne}
-                    submit={this.getResponse}
-                />
-            )
+            <div className="Quiz">
+                {
+                    QuestionsDb.map(
+                        q => 
+                        <Question
+                            key={q.question}
+                            question={q.question}
+                            options={q.options}
+                            selectOne={q.selectOne}
+                            submit={this.getResponse}
+                        />
+                    )
+                }
+                <button onClick={this.submitResponses}>Submit</button>
+            </div>
         )
     }
 }
