1f2565df2c8d1c942e4f06826a8a0590baf53bb3
Matthew VH <vanheckematt@gmail.com>
2020-06-16 14:27:58 -0500

    Added ArtDb.js which currently contains a collection of Movies each of which will include properties of that movie that will link users responses to it as well as some basic info on the movie. There will be similar collections for books, tv shows, and probably visual art.

---
+++ src/ArtDb.js
@@ -0,0 +1,206 @@
+const Movies = [
+    {
+        title: "Just Mercy",
+        properties: [
+            "Discouraged/Lethargic",
+            "Existential",
+            "A Movie",
+            "Activism/Polotics",
+            "Leslie Knope",
+            "Frodo Baggins",
+            "Sam Gamgee",
+            "Harry Potter",
+            "Captain America",
+            "Walter Mitty",
+            "Chuck Bartowski",
+            "Perspective",
+            "Motivation"
+        ]
+    },
+    {
+        title: "Hamilton",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Wonder Woman",
+        properties: [
+
+        ]
+    },
+    {
+        title: "The Lord of the Rings",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Mr. Smith Goes to Washington",
+        properties: [
+
+        ]
+    },
+    {
+        title: "A Hidden Life",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Amazing Grace",
+        properties: [
+
+        ]
+    },
+    {
+        title: "A Quiet Place",
+        properties: [
+
+        ]
+    },
+    {
+        title: "A Man for all Seasons",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Harry Potter",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Dunkirk",
+        properties: [
+            "Overwhelmed/Confused",
+            "Sad/Lonely",
+            "Praying",
+            "Discouraged/Lethargic",
+            "A Movie",
+            "First Responder/Military",
+            "Ben Wyatt",
+            "Frodo Baggins",
+            "Sam Gamgee",
+            "Merry Brandybuck",
+            "Pippin Took"
+        ]
+    },
+    {
+        title: "Arrival",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Jojo Rabbit",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Avengers: Endgame",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Tangled",
+        properties: [
+
+        ]
+    },
+    {
+        title: "The Lion King",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Black Panther",
+        properties: [
+
+        ]
+    },
+    {
+        title: "It's a Wonderful Life",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Joyeaux Noel",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Little Women",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Prince of Egypt",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Jumanji: Into the Jungle",
+        properties: [
+
+        ]
+    },
+    {
+        title: "The Sound of Music",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Shawshank Redemption",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Good Will Hunting",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Hacksaw Ridge",
+        properties: [
+
+        ]
+    },
+    {
+        title: "A Beautiful Day in the Neighborhood",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Anne of Green Gables",
+        properties: [
+
+        ]
+    },
+    {
+        title: "Elizabethtown",
+        properties: [
+
+        ]
+    },
+    {
+        title: "X-Men: Days of Future Past",
+        properties: [
+
+        ]
+    }
+]
+
+export {Movies};
